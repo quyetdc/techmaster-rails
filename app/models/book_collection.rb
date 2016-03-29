aa583f3467cd73e2006fd2efe5b2a9042ff199ba
ColinDao <quyetdc.uet@gmail.com>
2016-03-29 10:08:50 +0700

    custom validations

--- app/models/book_collection.rb
+++ app/models/book_collection.rb
@@ -1,4 +1,9 @@
 class BookCollection < ActiveRecord::Base
   belongs_to :book
   belongs_to :collection
+
+  validates :book_id, uniqueness: {
+                        scope: :collection_id,
+                        message: 'We should not add a book to a collection two times'
+                      }
 end
