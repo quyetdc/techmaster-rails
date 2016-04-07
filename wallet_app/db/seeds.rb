ead97bf92a4f9023a3f3915d209524b2f8765345
classicboy <classicboy189@gmail.com>
2016-04-07 14:33:47 +0700

    Add data

--- wallet_app/db/seeds.rb
+++ wallet_app/db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories_data = [
+    {
+        name: "Laptop"
+    },
+    {
+        name: "Mobilephone"
+    },
+    {
+        name: "Work"
+    }
+]
+
+categories_data.each do |c|
+  Category.create(name: c[:name])
+end
+
+
+incomes_data = [
+    {
+        amount: 2000,
+        issued_at: "01-04-2016",
+    },
+    {
+        amount: 1000,
+        issued_at: "01-04-2016",
+    }
+]
+
+incomes_data.each do |i|
+  Income.create(amount: i[:amount], issued_at: i[:issued_at])
+end
+
+
+costs_data = [
+    {
+        amount: 250,
+        issued_at: "2016-02-23",
+    },
+    {
+        amount: 1500,
+        issued_at: "2016-03-23",
+    }
+]
+
+costs_data.each do |c|
+  Cost.create(amount: c[:amount], issued_at: c[:issued_at])
+end
