e6b4f589598608861f7726bda3ef6a63083b644b
classicboy <classicboy189@gmail.com>
2016-04-08 14:07:29 +0700

    Change piechart to columnchart

--- wallet_app/app/controllers/pages_controller.rb
+++ wallet_app/app/controllers/pages_controller.rb
@@ -36,4 +36,59 @@ class PagesController < ApplicationController
     render :welcome
   end
 
+  def month_categories_piechart month, year
+    income_data = month_income_data month, year
+    cost_data = month_costs_data month, year
+
+    data = []
+    income_array = []
+    cost_array = []
+
+    categories_names = Category.all.map { |c| c.name }
+
+    categories_names.each do |cat|
+      ic_amount = 0
+      c_amount = 0
+
+      income_data.each do |_ic_data|
+        if _ic_data[:name] == cat
+          ic_amount = _ic_data[:y]
+          break
+        end
+      end
+
+      cost_data.each do |_c_data|
+        if _c_data[:name] == cat
+          c_amount = _c_data[:y]
+          break
+        end
+      end
+      data << { name: cat, income: ic_amount, cost: c_amount}
+    end
+
+    data.each do |d|
+      income_array << d[:income]
+      cost_array << d[:cost]
+    end
+
+    # TODO: CHANGE this to column chart
+    if data.count > 0
+      categories_chart = LazyHighCharts::HighChart.new('pie') do |f|
+        f.title(text: "Your Categories Finance in #{month}, #{year}")
+        f.xAxis(categories: (Category.all.map { |t| t.name }))
+        f.series(name: "Incomes", yAxis: 0, data: income_array)
+        f.series(name: "Costs", yAxis: 1, data: cost_array)
+
+        f.yAxis [
+                    {title: {text: "Incomes", margin: 70}},
+                    {title: {text: "Costs"}, opposite: true},
+                ]
+
+        f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
+        f.chart({defaultSeriesType: "column"})
+      end
+    end
+
+    categories_chart
+  end
 end
