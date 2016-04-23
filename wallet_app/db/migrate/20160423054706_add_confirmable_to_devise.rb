a396ff2a35f2b14ed040d449a03bcb47f6e346fe
ColinDao <quyetdc.uet@gmail.com>
2016-04-23 13:46:48 +0700

    Update db users for confirmable

--- wallet_app/db/migrate/20160423054706_add_confirmable_to_devise.rb
+++ wallet_app/db/migrate/20160423054706_add_confirmable_to_devise.rb
@@ -1,8 +1,12 @@
 class AddConfirmableToDevise < ActiveRecord::Migration
-  def change
-    change_table(:users) do |t|
-      t.confirmable
-    end
-    add_index  :users, :confirmation_token, :unique => true
+  def up
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_index :users, :confirmation_token, unique: true
+  end
+
+  def down
+    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
   end
 end
