db2e53c0b22b79efc3694772f57fa42ccc461a27
ColinDao <quyetdc.uet@gmail.com>
2016-05-26 10:58:32 +0700

    [Reschin] Complete Admin Banners

--- reschin/app/controllers/admin/banners_controller.rb
+++ reschin/app/controllers/admin/banners_controller.rb
@@ -10,7 +10,7 @@ class Admin::BannersController < ApplicationController
   end
 
   def create
-    @banner = Banner.create(banners_params)
+    @banner = Banner.create(banner_params)
 
     if @banner.save
       redirect_to admin_banners_path
@@ -19,9 +19,32 @@ class Admin::BannersController < ApplicationController
     end
   end
 
+  def show
+    @banner = Banner.find(params[:id])
+  end
+
+  def edit
+    @banner = Banner.find(params[:id])
+  end
+
+  def update
+    @banner = Banner.find(params[:id])
+    if @banner.update(banner_params)
+      redirect_to admin_banners_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @banner = Banner.find(params[:id])
+    @banner.destroy
+    redirect_to admin_banners_path
+  end
+
   private
 
-  def banners_params
+  def banner_params
     params.require(:banner).permit(:image, :name, :article_id, :description)
   end
 end
